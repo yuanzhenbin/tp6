5961635ad30e064d8895ba473824bad7f21ee926
yzb <1345993533@qq.com>
2022-10-09 21:41:54 +0800

    kafkademo

--- app/controller/KafkaController.php
+++ app/controller/KafkaController.php
@@ -11,7 +11,7 @@ use think\facade\View;
 
 class KafkaController extends BaseController
 {
-    public static $brokerList = '127.0.0.1:9002';
+    public static $brokerList = 'localhost:9092';
     public function index()
     {
         return View::fetch();
@@ -24,12 +24,19 @@ class KafkaController extends BaseController
         $conf->set('metadata.broker.list', self::$brokerList);
         $producer = new \RdKafka\Producer($conf);
         $topic = $producer->newTopic($topic);
-        $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($message));
-        $producer->poll(0);
-        $result = $producer->flush(10000);
-        if (RD_KAFKA_RESP_ERR_NO_ERROR !== $result) {
-            throw new \RuntimeException('Was unable to flush, messages might be lost!');
+//        while (true) {
+//            echo "\nEnter  messages:\n";
+//            //发消息
+//            $topic->produce(RD_KAFKA_PARTITION_UA, 0, 'message:'.$message);
+//        }
+
+        for ($i=1; $i<=3; $i++){
+            //发消息
+            sleep(1);
+            $topic->produce(RD_KAFKA_PARTITION_UA, 0, 'message:'.$message);
         }
+
+        return_ajax([],200,'done');
     }
 
     //消费者
@@ -40,28 +47,35 @@ class KafkaController extends BaseController
             return_ajax([],0,'话题不能为空');
         }
         $conf = new \RdKafka\Conf();
-        $conf->set('group.id', $topic);
+        $conf->set('group.id', 'myConsumerGroup');
+        $conf->set('enable.partition.eof', 'true');
+
         $rk = new \RdKafka\Consumer($conf);
-        $rk->addBrokers("127.0.0.1");
+        $rk->addBrokers("localhost:9092");
+
         $topicConf = new \RdKafka\TopicConf();
-        $topicConf->set('auto.commit.interval.ms', 100);
+        $topicConf->set('auto.commit.interval.ms', '100');
         $topicConf->set('offset.store.method', 'broker');
-        $topicConf->set('auto.offset.reset', 'smallest');
-        $topic = $rk->newTopic($topic, $topicConf);
-        $topic->consumeStart(0, RD_KAFKA_OFFSET_STORED);
+        $topicConf->set('auto.offset.reset', 'earliest');
+
+        $topic = $rk->newTopic("test", $topicConf);
+        $topic->consumeStart(0, RD_KAFKA_OFFSET_END);//RD_KAFKA_OFFSET_STORED
+
         while (true) {
+            // 第一个参数是分区，第二个参数是超时时间
             $message = $topic->consume(0, 120*10000);
             switch ($message->err) {
                 case RD_KAFKA_RESP_ERR_NO_ERROR:
-                    var_dump($message);
+                    var_dump($message->payload);
                     break;
                 case RD_KAFKA_RESP_ERR__PARTITION_EOF:
-                    echo "No more messages; will wait for more\n";
+                    echo "等待新消息\n";
                     break;
                 case RD_KAFKA_RESP_ERR__TIMED_OUT:
-                    echo "Timed out\n";
+                    echo "超时\n";
                     break;
                 default:
+                    //错误信息
                     throw new \Exception($message->errstr(), $message->err);
                     break;
             }
